6a7b48c192b13281e7be1d5ce8309f5d27fc63b9
mariannareis <mari.ounaum@gmail.com>
2011-07-21 20:24:05 -0300

    commit inicial, já adicionado devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clibr::Application.config.secret_token = '7df156ccbf09f6060285848176b0d3ff006cd412dd705f813e80855b8f03ca477dcef26756f9f335ea4ae1bcda59c626340fd997834d03cd197b69c7476d7657'
